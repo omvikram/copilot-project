2b653cf81396c7136d9fad33e2993b14fec719f5
omvikram <omvikram@hotmail.com>
2025-05-15 14:55:10 +0530

    hello copilot

---
+++ hello-copliot.js
@@ -0,0 +1,28 @@
+class calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(a, b) {
+    this.result = a + b;
+    return this.result;
+  }
+
+  subtract(a, b) {
+    this.result = a - b;
+    return this.result;
+  }
+
+  multiply(a, b) {
+    this.result = a * b;
+    return this.result;
+  }
+
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result = a / b;
+    return this.result;
+  }
+}
