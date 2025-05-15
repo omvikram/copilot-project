536b5b971aaa850232a1b0ab365296af40763266
omvikram <omvikram@hotmail.com>
2025-05-15 17:25:09 +0530

    added comments

--- hello-copliot.js
+++ hello-copliot.js
@@ -1,5 +1,7 @@
+// PercentageCalculator class provides utility methods for percentage calculations
 class PercentageCalculator {
     // Calculates what percent 'a' is of 'b'
+    // If process.env.B_VALUE is set, uses its value as 'b'
     percentOf(a, b) {
         const b = process.env.B_VALUE ? Number(process.env.B_VALUE) : b;
         if (b === 0) {
@@ -8,17 +10,17 @@ class PercentageCalculator {
         return (a / b) * 100;
     }
 
-    // Calculates 'percent'% of 'number'
+    // Calculates the value that is 'percent' percent of 'number'
     percentValue(percent, number) {
         return (percent / 100) * number;
     }
 
-    // Increases 'number' by 'percent'%
+    // Increases 'number' by 'percent' percent
     increaseByPercent(number, percent) {
         return number + this.percentValue(percent, number);
     }
 
-    // Decreases 'number' by 'percent'%
+    // Decreases 'number' by 'percent' percent
     decreaseByPercent(number, percent) {
         return number - this.percentValue(percent, number);
     }
